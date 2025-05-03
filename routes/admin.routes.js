13fec725390e51758036b4c21139786a25c8b9bd
aletorrescamacho <alessandra.torres@correo.unimet.edu.ve>
2025-05-02 22:32:33 -0400

    modificacion 2

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -179,5 +179,5 @@ router.get('/estadisticas/:slug/citas-por-anio', EstadisticasController.citasPor
 
 
 
-router.get("/citas/:slug/total", getTotalCitas)
+router.get("/citas/:slug/total", adminController.getTotalCitas)
 module.exports = router;
